defab363bcb9c498e3056302164242934e5d7cca
Aadi Rave <arave@purdue.edu>
2025-12-07 17:39:39 -0500

    added typst enhancements

--- init.lua
+++ init.lua
@@ -1142,10 +1142,11 @@ require("tiny-inline-diagnostic").setup({
 
 local Rule = require("nvim-autopairs.rule")
 local npairs = require("nvim-autopairs")
+local ts_conds = require("nvim-autopairs.ts-conds")
 
 npairs.add_rule(Rule("$", "$", { "typ", "typst" }))
 npairs.add_rule(Rule("*", "*", { "typ", "typst" }))
-npairs.add_rule(Rule("_", "_", { "typ", "typst" }))
+npairs.add_rule(Rule("_", "_", { "typ", "typst" }):with_pair(ts_conds.is_not_ts_mode({ "math" })))
 
 -- typst autoformat
 
